91fbdfa35ee7658975529457d9eda04e37e52ec7
Damien Hampton <damien@26brains.com>
2024-03-23 17:12:53 +0000

    Added movie db contract tests

---
+++ src/movie-db-contract.spec.ts
@@ -0,0 +1,48 @@
+import { MovieDBInterface } from "./movie-db";
+import { MovieDBRecord } from "./movie-model";
+
+type CreateTestMovieDBParams = MovieDBRecord[];
+
+export function movieDBContractTests(
+  name: string,
+  createTestMovieDB: (params: CreateTestMovieDBParams) => MovieDBInterface
+) {
+  describe(`${name}: movie db contract tests`, () => {
+    context("getMovie", () => {
+      const movieDb = createTestMovieDB([
+        {
+          id: 1234,
+          name: "The TDD movie",
+          votes: 5,
+          tmdbId: 2345,
+        },
+        {
+          id: 5678,
+          name: "The BDD movie",
+          votes: 4,
+          tmdbId: 6789,
+        },
+      ]);
+      context("with movieId 1234", () => {
+        it("should return correct movie", async () => {
+          expect(await movieDb.getMovie(1234)).to.deep.equal({
+            id: 1234,
+            name: "The TDD movie",
+            votes: 5,
+            tmdbId: 2345,
+          });
+        });
+      });
+      context("with movieId 5678", () => {
+        it("should return correct movie", async () => {
+          expect(await movieDb.getMovie(5678)).to.deep.equal({
+            id: 5678,
+            name: "The BDD movie",
+            votes: 4,
+            tmdbId: 6789,
+          });
+        });
+      });
+    });
+  });
+}
